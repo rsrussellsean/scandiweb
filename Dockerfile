0649bb26189d7c9f5386f7ddc02c52b65db5b4a9
Russell Sean <russell.sean.rs@gmail.com>
2025-06-30 22:29:59 +0800

    added docker

--- Dockerfile
+++ Dockerfile
@@ -14,3 +14,5 @@ RUN composer install --no-dev --optimize-autoloader
 
 RUN chown -R www-data:www-data /var/www/html
 EXPOSE 80
+
+RUN sed -i 's|/var/www/html|/var/www/html/public|g' /etc/apache2/sites-available/000-default.conf
--- public/index.php
+++ public/index.php
@@ -1,37 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
-    $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
-});
-
-$routeInfo = $dispatcher->dispatch(
-    $_SERVER['REQUEST_METHOD'],
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-switch ($routeInfo[0]) {
-    case FastRoute\Dispatcher::NOT_FOUND:
-        http_response_code(404);
-        echo json_encode(['error' => 'Not found']);
-        break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        http_response_code(405);
-        echo json_encode(['error' => 'Method not allowed']);
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        [$class, $method] = $routeInfo[1];
-        $vars = $routeInfo[2];
-        $controller = new $class();
-        echo $controller->$method($vars);
-        break;
-}
+echo "PHP is working 🚀";
